7d8e979a1e263f3c6d82a64f51b2d6ed18cf78e7
ecopowerrafael <ecopowerrafael@users.noreply.github.com>
2026-04-04 15:54:08 -0300

    fix(android): encaminha Notificacao Nativa para o CallKit para acordar tela com app fechado

--- android/app/src/main/java/com/astria/taromobile/PushNotificationService.java
+++ android/app/src/main/java/com/astria/taromobile/PushNotificationService.java
@@ -4,12 +4,25 @@ import androidx.annotation.NonNull;
 
 import com.capacitorjs.plugins.pushnotifications.MessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.hiennv.flutter_callkit_incoming.FlutterCallkitIncomingPlugin;
 
 public class PushNotificationService extends MessagingService {
 
     @Override
-    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {       
         super.onMessageReceived(remoteMessage);
+        
+        // Repassa a notificação para o CallKit Nativo interceptar e acordar a tela
+        try {
+            FlutterCallkitIncomingPlugin.Companion.sendRemoteMessage(remoteMessage, this.getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Se for uma ligação nativa do CallKit, não precisa criar notificação padrão
+        if (remoteMessage.getData().containsKey("call")) {
+            return;
+        }
 
         if (remoteMessage.getData().isEmpty()) {
             return;
@@ -21,5 +34,9 @@ public class PushNotificationService extends MessagingService {
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
-    }
-}
+        
+        try {
+            FlutterCallkitIncomingPlugin.Companion.onNewToken(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
